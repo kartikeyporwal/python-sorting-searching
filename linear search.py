10c4b054c50e53d300b14146b83bab05900380d3
Kartikey Porwal <kartikey.porwal@gmail.com>
2018-05-25 19:57:16 +0530

    Initial commit with linear search

---
+++ linear search.py
@@ -0,0 +1,21 @@
+#Defining a function for linear search which take a list of items and a item to be searched as an input
+
+#importing shuffle from random module to shuffle list elements
+from random import shuffle
+
+def linear_search(alist, item):
+    
+    position = 0
+    found = False
+    while position < len(alist) and found == False:
+        if alist[position] == item:
+            found = True
+        else:
+            position += 1
+            
+    return found
+
+a = [i for i in range(200)]
+shuffle(a)
+
+print(linear_search(a, 76))
