c8482d37b952892a3c970f72d7d9665c00a0a9d4
Kartikey Porwal <kartikey.porwal@gmail.com>
2018-05-28 03:47:39 +0530

    Bubble Sort Implementation

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble(alist):
+    for passes in range(len(alist)-1, 0, -1):
+        for i in range(passes):
+            if alist[i]>alist[i+1]:
+                alist[i], alist[i+1] = alist[i+1], alist[i]
+
+    return alist
+
+print(bubble([12,34,24,23,2,34,1,24,56,32,45]))
+
+#Bubble sort offers O(n^2) in worst case & average case. In best case it offers
+# O(n) because no exchange is done.
