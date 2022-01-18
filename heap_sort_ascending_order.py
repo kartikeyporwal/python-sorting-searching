eb6d0f25cc43deb63503ee2885c3ad89edc21597
Kartikey Porwal <kartikey.porwal@gmail.com>
2022-01-18 23:57:03 +0530

    - Add heap sort implementation to sort the array in the ascending order

---
+++ heap_sort_ascending_order.py
@@ -0,0 +1,19 @@
+import heapq
+
+array_to_sort = [54, 14, 549, 365, 74, 4, 25, 34]
+
+# initialise an empty array that will contain the sorted data
+sorted_array = []
+
+# heapify the array
+heapq.heapify(array_to_sort)
+
+# process until there is no element left in the array to be sorted
+while array_to_sort:
+    smallest_element = heapq.heappop(array_to_sort)
+    sorted_array.append(smallest_element)
+
+# print the sorted array
+# Output [4, 14, 25, 34, 54, 74, 365, 549]
+print(sorted_array)
+
